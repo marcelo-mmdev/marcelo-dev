3754c7b4934cc17ad5484876c1d44d675a56d3fe
Marcelo Rodrigues <85708828+marcelo-mmdev@users.noreply.github.com>
2024-05-13 23:09:25 -0300

    feature: Iniciando o header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,48 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter, IBM_Plex_Mono } from 'next/font/google'
+import './globals.css'
+// import { Header } from './components/header'
+import { ReactNode } from 'react'
+import { Header } from '@/components/header'
+// import { ContactForm } from './components/contact-form'
+// import { Footer } from './components/footer'
+// import { Toaster } from './components/toaster'
+// import { BackToTop } from './components/back-to-top'
 
-const inter = Inter({ subsets: ["latin"] });
+export const metadata = {
+  title: {
+    default: 'Home',
+    template: '%s | GB Dev',
+  },
+  icons: [
+    {
+      url: '/favicon.svg',
+    },
+  ],
+}
 
-export const metadata: Metadata = {
-  title: "Marcelo Rodrigues",
-  description: "Portifólio next app",
-};
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+})
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const plexMono = IBM_Plex_Mono({
+  variable: '--font-plex-mono',
+  subsets: ['latin'],
+  weight: ['400', '500'],
+})
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR" className={`${inter.variable} ${plexMono.variable}`}>
+      <body>
+        {/* <Toaster />
+        <BackToTop /> */}
+
+        <Header />
+        {children}
+        {/* <ContactForm />
+        <Footer /> */}
+      </body>
     </html>
-  );
+  )
 }
---
+++ src/components/header/index.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import Image from "next/image";
+import Link from "next/link";
+import { NavItem } from './nav-item';
+
+const NAV_ITENS = [
+  {
+    label: "Home",
+    href: "/"
+  },
+  {
+    label: "Projetos",
+    href: "/projects"
+  },
+  {
+    label: "Contatos",
+    href: "/contatcs"
+  }
+]
+
+export const Header = () => {
+  return (
+    <header className='absolute top-0 w-full z-10 h-24 flex items-center justify-center'>
+      <div className="container flex items-center justify-between">
+        <Link href="/">
+          <Image
+            width={58}
+            height={49}
+            src="images/next.svg"
+            alt="logo MMdev"
+          />
+        </Link>
+
+        <nav className='flex items-center gap-4 sm:gap-10'>
+          {NAV_ITENS.map(item => (
+            <NavItem {...item} key={item.label} />
+          ))}
+        </nav>
+      </div>
+    </header>
+  );
+};
